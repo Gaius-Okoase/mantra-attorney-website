4b076602b2219b14eeabf5f6c141aaa13d21224e
Gaius Okoase <gaiusonosetale@gmail.com>
2025-05-21 02:11:54 +0100

    Add mongoose dependency and implement MongoDB connection logic

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+// Import mongoose to connect with MongoDB
+import mongoose from 'mongoose';
+
+// Code logic to connect with MongoDB
+export const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Successfully connected with MongoDB.")
+    } catch (error) {
+        console.log("Couldn't connect with MongoDB", error);
+        process.exit(1);
+    };
+};
+
+
